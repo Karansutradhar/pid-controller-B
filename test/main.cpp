e5d49d710ff083eb3ecb262abdfb87ad9c38ae81
--replace-all <vishnuu@terpmail.umd.edu>
2020-09-29 16:34:54 -0400

    Modified spacing and added copyright for main.cpp in test

--- test/main.cpp
+++ test/main.cpp
@@ -1,32 +1,32 @@
-/**
-* @file main.cpp
-* @authors
-*
-* Part 1
-* Karan Sutradhar (117037272) - Driver
-* Vishnuu Appaya Dhanabalan (116873314) - Navigator
-*
-* Part 2
-* Sukoon Sarin (sukoonsarin) - Navigator
-* Nalin Das (nalindas9) -  Driver
-*
-* @version 1.0
-*
-* @section LICENSE
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License as
-* published by the Free Software Foundation; either version 2 of
-* the License, or (at your option) any later version.
-*
-* @section DESCRIPTION
-*
-*  This is main.cpp file for the test this file implements
-*  the unit test cases
-*/
+/** Copyright 2020 Karan Sutradhar , Vishnuu A D
+ * @file main.cpp
+ * @authors
+ *
+ * Part 1
+ * Karan Sutradhar (117037272) - Driver
+ * Vishnuu Appaya Dhanabalan (116873314) - Navigator
+ *
+ * Part 2
+ * Sukoon Sarin (sukoonsarin) - Navigator
+ * Nalin Das (nalindas9) -  Driver
+ *
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @section DESCRIPTION
+ *
+ *  This is main.cpp file for the test this file implements
+ *  the unit test cases
+ */
 
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 /**
  * @brief Default main function to run the test blocks
@@ -35,8 +35,7 @@
  * @returns test values
  */
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
