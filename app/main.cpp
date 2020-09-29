405f40f0ffba255e082507c9eb567bc46d44842f
--replace-all <vishnuu@terpmail.umd.edu>
2020-09-29 16:34:03 -0400

    Modified spacing and added copyright main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,62 +1,64 @@
-/**
-* @file main.cpp
-* @authors
-*
-* Part 1
-* Karan Sutradhar (117037272) - Driver
-* Vishnuu Appaya Dhanabalan (116873314) - Navigator
-* 
-* Part 2
-* Sukoon Sarin (sukoonsarin) - Navigator
-* Nalin Das (nalindas9) -  Driver
-*
-* @date 9/27/2020
-* @version 1.0
-*
-* @section LICENSE
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License as
-* published by the Free Software Foundation; either version 2 of
-* the License, or (at your option) any later version.
-*
-* @section DESCRIPTION
-*
-*  This is main.cpp file for PID Controller this file implements
-*  the code to compute the velocity with minimum error and works
-*  on feedback loop to maintain velocity to required velocity
-*/
+/** Copyright 2020 Karan Sutradhar , Vishnuu A D
+ * @file main.cpp
+ * @authors
+ *
+ * Part 1
+ * Karan Sutradhar (117037272) - Driver
+ * Vishnuu Appaya Dhanabalan (116873314) - Navigator
+ *
+ * Part 2
+ * Sukoon Sarin (sukoonsarin) - Navigator
+ * Nalin Das (nalindas9) -  Driver
+ *
+ * @date 9/27/2020
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @section DESCRIPTION
+ *
+ *  This is main.cpp file for PID Controller this file implements
+ *  the code to compute the velocity with minimum error and works
+ *  on feedback loop to maintain velocity to required velocity
+ */
 
-#include <iostream>
 #include <cmath>
+#include <iostream>
+
 #include "../include/PIDController.h"
 
 /**
- * @brief It is the main function to demonstrate working of implemented controller.
+ * @brief It is the main function to demonstrate working of implemented
+ * controller.
  * @param none
  * @returns 0
  */
 
-int main()
-{
-    double kp = 0.02;
-    double ki = 0.002;
-    double kd = 0.002;
-    double dt = 0.2;
-    double setPoint = 5;
-    double feedback = 0;
-    double tolerance = 0.1;
-    tdd::pidController pid(kp, ki, kd, dt); // Initializing the constructor with the input values of kp, ki, kd, dt
-    std::cout << "PID GAINS For the simulation" << std::endl;
-    std::cout << "kp: " << kp << ", ki: " << ki << ", kd: " << kd << ", dt: " << dt << std::endl;
-    std::cout << "Error Tolerance: " << tolerance << std::endl;
-    std::cout << "SetPoint Velocity: " << setPoint << ", Initial Velocity: " << feedback << std::endl;
+int main() {
+  double kp = 0.02;
+  double ki = 0.002;
+  double kd = 0.002;
+  double dt = 0.2;
+  double setPoint = 5;
+  double feedback = 0;
+  double tolerance = 0.1;
+  tdd::pidController pid(kp, ki, kd, dt);  // Initializing the constructor with
+                                           // the input values of kp, ki, kd, dt
+  std::cout << "PID GAINS For the simulation" << std::endl;
+  std::cout << "kp: " << kp << ", ki: " << ki << ", kd: " << kd
+            << ", dt: " << dt << std::endl;
+  std::cout << "Error Tolerance: " << tolerance << std::endl;
+  std::cout << "SetPoint Velocity: " << setPoint
+            << ", Initial Velocity: " << feedback << std::endl;
 
-    while (abs(setPoint - feedback) > abs(tolerance))
-    {
-        feedback = pid.calculateVelocity(setPoint, feedback);
-        std::cout << "Feedback: " << feedback << std::endl;
-    }
-    std::cout << std::endl
-              << "Controller Converged!" << std::endl;
+  while (abs(setPoint - feedback) > abs(tolerance)) {
+    feedback = pid.calculateVelocity(setPoint, feedback);
+    std::cout << "Feedback: " << feedback << std::endl;
+  }
+  std::cout << std::endl << "Controller Converged!" << std::endl;
 }
