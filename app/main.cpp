97c753e4e152cfdb533f0accf5471fa8e9602045
Karan Sutradhar <60763520+Karansutradhar@users.noreply.github.com>
2020-09-29 13:57:04 -0400

    Updated comments and authors

--- app/main.cpp
+++ app/main.cpp
@@ -2,9 +2,15 @@
 * @file main.cpp
 * @authors
 *
-* Karan Sutradhar (117037272)
-* Vishnuu Appaya Dhanabalan (116873314)
+* Part 1
+* Karan Sutradhar (117037272) - Driver
+* Vishnuu Appaya Dhanabalan (116873314) - Navigator
+* 
+* Part 2
+* Sukoon Sarin (sukoonsarin) - Navigator
+* Nalin Das (nalindas9) -  Driver
 *
+* @date 9/27/2020
 * @version 1.0
 *
 * @section LICENSE
